d607a5864c5c4132b593fd796991fb6bb94097b5
Faska <hichamfaska50@gmail.com>
2026-01-16 16:20:39 +0100

    connexion a la base de donnée

---
+++ Database.php
@@ -0,0 +1,10 @@
+<?php
+    namespace App\Database;
+    use PDO;
+
+    class Database{
+        public function __construct()
+        {
+            throw new \Exception('Not implemented');
+        }
+    }
