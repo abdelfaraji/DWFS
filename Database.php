dc45a2053cd3fc094361f56a05e91a3cebcd953e
abdelfaraji <farajiabdel63@gmail.com>
2026-01-16 16:49:42 +0100

    Update Database.php

--- Database.php
+++ Database.php
@@ -5,7 +5,8 @@
     class Database{
         private $host = "localhost";
         public function __construct()
-        { // instanciation de la classe PDO
+        
+        { // instanciation de la classe PDO pour connexion
             throw new \Exception('Not implemented');
         }
     }
